33a21facdf1da856313ae7165f6a46fb5c7ab48d
111 <1142540213@qq.com>
2026-04-16 10:39:28 +0800

    Add files via upload

--- system/king1/frontend/vite.config.js
+++ system/king1/frontend/vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url' // 1. 引入 node 的 url 模块
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  // 2. 添加 resolve.alias 配置
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
   server: {
     port: 3000,
     proxy: {
